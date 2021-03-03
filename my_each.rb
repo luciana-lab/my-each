89b292b0c62442712893044e4ec2372e5b4d1c0a
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-02 19:54:31 -0500

    commit tree b7c7598f4c161ca1855f3cf51dc65c9549dfcf51

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+words = []
+def my_each(words) # put argument(s) here
   # code here
+i = 0
+while i < words.size
+  yield(words[i])
+  i += 1
+end
+words
+end
+
+my_each(words) do |word|
+  word
 end
